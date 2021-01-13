a9c8de6af018977bf5dfc92fc30cde313e56c588
AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
2021-01-13 19:38:10 +0100

    clk: qcom: gcc-sdm660: Mark GPU CFG AHB clock as critical
    This clock is critical for any access to the GPU: gating it will
    crash the system when the GPU has been initialized (so, you cannot
    gate it unless you deinit the Adreno completely).
    
    So, to achieve a working state with GPU on, set the CLK_IS_CRITICAL
    flag to this clock.
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
    Link: https://lore.kernel.org/r/20210113183817.447866-3-angelogioacchino.delregno@somainline.org
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>

--- drivers/clk/qcom/gcc-sdm660.c
+++ drivers/clk/qcom/gcc-sdm660.c
@@ -1749,6 +1749,7 @@ static struct clk_branch gcc_gpu_cfg_ahb_clk = {
 		.hw.init = &(struct clk_init_data){
 			.name = "gcc_gpu_cfg_ahb_clk",
 			.ops = &clk_branch2_ops,
+			.flags = CLK_IS_CRITICAL,
 		},
 	},
 };
