28a5fde34f44b65013c87a617e6496be2e2aedc3
Vixel View <vcyzscape@gmail.com>
2021-11-30 23:32:08 +0700

    power: qcom: chg jeita decrease temp pwr supply
    i don't know how much this this work but i have reference this commit from commit kerneltoast

--- drivers/power/supply/qcom/step-chg-jeita.c
+++ drivers/power/supply/qcom/step-chg-jeita.c
@@ -88,9 +88,9 @@ static struct step_chg_cfg step_chg_config = {
 	.hysteresis	= 100000, /* 100mV */
 	.fcc_cfg	= {
 		/* VBAT_LOW	VBAT_HIGH	FCC */
-		{3600000,	4000000,	3000000},
-		{4001000,	4200000,	2800000},
-		{4201000,	4400000,	2000000},
+		{3600000,	3800000,	3000000},
+		{3801000,	4000000,	3000000},
+		{4001000,	4200000,	3000000},
 	},
 	/*
 	 *	SOC STEP-CHG configuration example.
@@ -121,10 +121,10 @@ static struct jeita_fcc_cfg jeita_fcc_config = {
 	.hysteresis	= 10, /* 1degC hysteresis */
 	.fcc_cfg	= {
 		/* TEMP_LOW	TEMP_HIGH	FCC */
-		{0,		100,		600000},
-		{101,		200,		2000000},
-		{201,		450,		3000000},
-		{451,		550,		600000},
+		{0,		100,		3000000},
+		{101,		200,		3000000},
+		{201,		400,		3000000},
+		{451,		420,		3000000},
 	},
 };
 
@@ -135,8 +135,8 @@ static struct jeita_fv_cfg jeita_fv_config = {
 	.fv_cfg		= {
 		/* TEMP_LOW	TEMP_HIGH	FCC */
 		{0,		100,		4200000},
-		{101,		450,		4400000},
-		{451,		550,		4200000},
+		{101,		400,		4200000},
+		{451,		420,		4200000},
 	},
 };
 
