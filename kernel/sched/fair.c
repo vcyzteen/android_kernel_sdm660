131a2143ead6e45d3a775e12bc06f12e7ecd4e95
ʏᴜᴜ <vcyzscape@gmail.com>
2022-01-09 23:57:33 +0700

    sched: fair: minimize concurrent LBs between domain level
    sched domains tend to trigger simultaneously the load balance loop but
    the larger domains often need more time to collect statistics. This
    slowness makes the larger domain trying to detach tasks from a rq whereas
    tasks already migrated somewhere else at a sub-domain level. This is not
    a real problem for idle LB because the period of smaller domains will
    increase with its CPUs being busy and this will let time for higher ones
    to pulled tasks. But this becomes a problem when all CPUs are already busy
    because all domains stay synced when they trigger their LB.
    
    A simple way to minimize simultaneous LB of all domains is to decrement the
    the busy interval by 1 jiffies. Because of the busy_factor, the interval of
    larger domain will not be a multiple of smaller ones anymore.
    
    commit from: https://lwn.net/ml/linux-kernel/20200914100340.17608-4-vincent.guittot@linaro.org/
    cc: Vincent Guittot <vincent.guittot@linaro.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10778,6 +10778,9 @@ get_sd_balance_interval(struct sched_domain *sd, int cpu_busy)
 
 	/* scale ms to jiffies */
 	interval = msecs_to_jiffies(interval);
+        if (cpu_busy)
+		interval -= 1;
+
 	interval = clamp(interval, 1UL, max_load_balance_interval);
 
 	return interval;
