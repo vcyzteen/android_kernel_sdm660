b67174ed328c1a7c5a9b49b973311d6302ec79d5
ʏᴜᴜ <vcyzteen@protonmail.com>
2022-02-12 14:35:27 +0700

    sound: soc: pcm: increase pcm rate

--- sound/soc/msm/qdsp6v2/msm-pcm-q6-noirq.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-q6-noirq.c
@@ -117,9 +117,9 @@ static struct snd_pcm_hardware msm_pcm_hardware_capture = {
 	.formats =              (SNDRV_PCM_FMTBIT_S16_LE |
 				SNDRV_PCM_FMTBIT_S24_LE |
 				SNDRV_PCM_FMTBIT_S24_3LE),
-	.rates =                SNDRV_PCM_RATE_8000_48000,
+	.rates =                SNDRV_PCM_RATE_8000_96000,
 	.rate_min =             8000,
-	.rate_max =             48000,
+	.rate_max =             96000,
 	.channels_min =         1,
 	.channels_max =         4,
 	.buffer_bytes_max =     SIO_CAPTURE_MAX_NUM_PERIODS *
