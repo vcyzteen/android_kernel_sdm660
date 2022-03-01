3d2392aef52154c0e3b00b10e5fec269e937762f
ʏᴜᴜ <vcyzteen@protonmail.com>
2022-03-01 21:30:26 +0700

    pagealloc: improve little bit system performance
    Be careful when setting this parameter; too small or too large a value can adversely affect system performance. Too low value min_free_kbytes will prevent the system from freeing memory. Which can lead to system freeze or destruction of processes via OOM.
    
    Too large a value (5-10% of the total memory) will cause the system to quickly run out of memory. Linux uses all available RAM to cache file system data. Setting a high value min_free_kbytes can cause the system to spend too much time recovering memory.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -250,7 +250,7 @@ compound_page_dtor * const compound_page_dtors[] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 1024;
+int min_free_kbytes = 1153;
 int user_min_free_kbytes = -1;
 
 /*
